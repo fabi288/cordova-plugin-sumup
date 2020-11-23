19c31f3b6da9fde12fc6fb3c589d9461c8391f8b
fabi288 <info@fhars.de>
2020-11-23 08:56:54 +0100

    Update sumup.js

--- www/sumup.js
+++ www/sumup.js
@@ -1,3 +1,4 @@
+cordova.define("cordova-plugin-sumup.SumUp", function(require, exports, module) {
 // iOS
 // SMPSumUpSDKErrorGeneral             = 0,        // General error
 // SMPSumUpSDKErrorActivationNeeded    = 1,        // The merchant's account is not activated
@@ -8,28 +9,44 @@
 // SMPSumUpSDKErrorCheckoutInProgress  = 51,       // Another checkout process is currently in progress.
 
 module.exports = {
-    login: function (accessToken) {
-        return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'SumUp', 'login', accessToken ? [accessToken] : []));
-    },
-    auth: function (accessToken) {
-        return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'SumUp', 'auth', accessToken ? [accessToken] : []));
-    },
-    logout: function () {
-        return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'SumUp', 'logout', []));
-    },
-    settings: function () {
-        return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'SumUp', 'settings', []));
-    },
-    prepare: function (force) {
-        return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'SumUp', 'prepare', []));
-    },
-    close: function (force) {
-        return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'SumUp', 'close', []));
-    },
-    pay: function (amount, currencycode, title) {
-        return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'SumUp', 'pay', [amount, currencycode, title]));
-    },
-    isloggedin: function() {
-        return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'SumUp', 'isLoggedIn', []));
-    }
+	
+	
+	login: (accessToken, success, failure) => {
+    cordova.exec(
+      success,
+      failure,
+      "SumUp",
+      "login",
+      accessToken ? [accessToken] : []
+    );
+  },
+  auth: (accessToken, success, failure) => {
+    cordova.exec(
+      success,
+      failure,
+      "SumUp",
+      "auth",
+      accessToken ? [accessToken] : []
+    );
+  },
+  getSettings: (success, failure) => {
+    cordova.exec(success, failure, "SumUp", "getSettings", []);
+  },
+  logout: (success, failure) => {
+    cordova.exec(success, failure, "SumUp", "logout", []);
+  },
+  isLoggedIn: (success, failure) => {
+    cordova.exec(success, failure, "SumUp", "isLoggedIn", []);
+  },
+  prepare: (success, failure) => {
+    cordova.exec(success, failure, "SumUp", "prepare", []);
+  },
+  closeConnection: (success, failure) => {
+    cordova.exec(success, failure, "SumUp", "closeConnection", []);
+  },
+  pay: (amount, currencycode, title, success, failure) => {
+    cordova.exec(success, failure, "SumUp", "pay", [amount, currencycode, title]);
+  }
 }
+
+});
